e6bba7a4bb3b109b28ca0f0da9ac699b2604d7ce
vjeran fistric <vjeran@canelio.com>
2017-04-18 00:27:02 +0200

    1-src

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  // directive letting know component sort happened
+  onSort (data:any){
+    console.log("sorted data:", data);
+    console.log("whole data:",this.dataToSort);
+    console.log("------------------------");
+  }
+
+  // dummy data for our sort testing
+  dataToSort:any = [
+    {
+      name: 'Category 1',
+      children: [
+        { name: 'Item 1' },
+        {
+          name: 'item 2', children: [
+            'SubItem 1',
+            'SubItem 2'
+          ]
+        }
+      ]
+    },
+    {
+      name: 'Category 2', children: [
+        {
+          name: 'Item 3', children: [
+            'SubItem 1',
+            'SubItem 2'
+          ]
+        },
+        { name: 'Item 4' },
+        { name: 'Item 5' },
+      ]
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { JquerySortableDirective } from './jquery-sortable/jquery-sortable.directive';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -7,7 +8,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JquerySortableDirective
   ],
   imports: [
     BrowserModule,
